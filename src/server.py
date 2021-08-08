5ce69289ab0224e411ae1f97f5e59d42692d0c23
vad1mD <dolvadim93@gmail.com>
2021-08-08 16:49:23 +0300

    initial commit

---
+++ src/server.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, is it me you're looking for?"
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
