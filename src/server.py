1684f22c7aedcd111f7fd98fca2e8674547866dc
vad1mD <dolvadim93@gmail.com>
2021-08-09 12:27:58 +0300

    message changed

--- src/server.py
+++ src/server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, is it me you're looking for???"
+    return "Hello, is it me you're looking for"
 
 if __name__=="__main__":
     app.run(host='0.0.0.0')
