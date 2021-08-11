43538357f45929143637d871b548c86864b0422a
vadimdo <vadim.dolguintsev@amdocs.com>
2021-08-11 13:36:06 +0300

    fire added

--- src/server.py
+++ src/server.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import fire
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, pre demo testing"
+    return "Hello, is it me you looking for?"
 
 if __name__=="__main__":
     app.run(host='0.0.0.0')
